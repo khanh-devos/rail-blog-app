47ce6c0fa77e982c688ef796b0c785ed3fc460bf
khanh-devos <dreamproperty.khanh@gmail.com>
2023-10-05 17:19:34 +0700

    best practices with routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,18 @@
 Rails.application.routes.draw do
-  resources :likes
-  # resources :users
+  # resources :likes
+  resources :users do
+    resources :posts
+  end
   # resources :comments
-  # resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  get '/users', to: 'users#index'
-  get '/users/:id', to: 'users#show', as: 'show_a_user'
+  # get '/users', to: 'users#index'
+  # get '/users/:id', to: 'users#show', as: 'show_a_user'
   
-  get '/users/:user_id/posts', to: 'posts#index', as: 'show_a_user_posts_comments'
+  # get '/users/:user_id/posts', to: 'posts#index', as: 'show_a_user_posts_comments'
 
-  get '/users/:user_id/posts/:id', to: 'posts#show', as: 'show_a_user_a_post_comments'
+  # get '/users/:user_id/posts/:id', to: 'posts#show', as: 'show_a_user_a_post_comments'
 
   root 'users#index'
 
