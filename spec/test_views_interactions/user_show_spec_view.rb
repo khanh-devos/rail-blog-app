f35a6f1a50e310f2baec10372c9ddf16d29bdd4d
khanh-devos <dreamproperty.khanh@gmail.com>
2023-10-10 21:03:35 +0700

    Add '_spec' to test files' names

---
+++ spec/test_views_interactions/user_show_spec_view.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe 'users/show', type: :view do
+  before(:each) do
+    @user = User.create(name: 'John Doe')
+    render
+  end
+
+  it 'displays the header with "Recent Posts"' do
+    expect(rendered).to have_selector('h2', text: 'Recent Posts')
+    expect(rendered).to have_selector('img')
+  end
+
+  it 'renders the user partial' do
+    expect(rendered).to render_template(partial: '_user')
+  end
+
+  it 'displays a "Back to users" link' do
+    expect(rendered).to have_link('Back to users', href: users_path)
+  end
+end
